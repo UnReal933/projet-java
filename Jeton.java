8ac56adb7ee325491edd9df52bde012548e910da
BiBiCh3 <BiBiCh3@MacBook-Pro-de-Thomas-3.local>
2016-11-13 22:21:30 +0100

    Ajout fichier manquant

---
+++ Jeton.java
@@ -0,0 +1,62 @@
+/**
+ * Created by zulupero on 08/09/16.
+ */
+public class Jeton {
+    private int valeur;
+    private Plateau.couleur couleur;
+    private int position;
+    private double rayon ;
+
+    /**
+     * Constructeur initialisant l'ensemble des attributs.
+     * @param val valeur faciale du jeton
+     * @param col couleur du jeton
+     * @param r rayon du jeton
+     * @param pos position sur le plateau
+     */
+    public Jeton(int val, Plateau.couleur col, double r, int pos){
+        valeur = val;
+        couleur = col;
+        position = pos;
+        rayon = r;
+    }
+
+    /**
+     * Constructeur initialisant l'ensemble des atributs,
+     * sauf le rayon.
+     * @param val valeur faciale du jeton
+     * @param col couleur du jeton
+     * @param pos position sur le plateau
+     */
+    public Jeton(int val, Plateau.couleur col, int pos){
+        valeur = val;
+        couleur = col;
+        position = pos;
+    }
+
+    /**
+     * Constructeur initialisant l'ensemble des attributs,
+     * sauf la position, qui n'est pas nécessairement connue
+     * à la construction.
+     * @param val valeur faciale du jeton
+     * @param col couleur du jeton
+     * @param r rayon du jeton
+     */
+    public Jeton(int val, Plateau.couleur col, double r){
+        valeur = val;
+        couleur = col;
+        rayon = r;
+    }
+
+
+
+    /**
+     * Dessine le jeton sur le plateau
+     * @param x abcisse du jeton
+     * @param y ordonnée du jeton
+     */
+    public void trace(double x, double y){
+        /* à concevoir*/
+    }
+
+}
---
+++ Util.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+/**
+ * Created by zulupero on 20/09/16.
+ */
+public class Util {
+
+    private static Random random;
+
+
+}
